f636b2af9e449e49168cc96e570873d8406398c1
Matthias Ellero <matthias.ellero@etu.imt-nord-europe.fr>
2024-02-05 17:57:17 +0100

    Debut TP1

---
+++ TP1/lib/menuNav.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class menuNav extends StatelessWidget {
+    const menuNav({super.key})
+    @override
+    Widget build(BuildContext context){
+        return(
+            // TODO
+        )
+    }
+}
