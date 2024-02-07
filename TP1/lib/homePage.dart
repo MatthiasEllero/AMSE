3cd6edad5c87096433a3177c779e5c9d21599f4f
Matthias Ellero <matthias.ellero@etu.imt-nord-europe.fr>
2024-02-07 12:00:39 +0100

    modif media

--- TP1/lib/homePage.dart
+++ TP1/lib/homePage.dart
@@ -1,9 +1,10 @@
 import "package:flutter/material.dart";
+import "media.dart";
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    return (Text('test'));
+    return (Text(liste_media[0].name));
   }
 }
