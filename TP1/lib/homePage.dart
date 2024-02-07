a5b67e4dc531a8d48660c7fb895b4bef45e2b7a8
achraf205 <achrafouhadach@gmail.com>
2024-02-07 11:40:23 +0100

    essai

--- TP1/lib/homePage.dart
+++ TP1/lib/homePage.dart
@@ -1,23 +1,16 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 class HomePage extends StatelessWidget {
-    const HomePage({super.key});
-    @override
-    Widget build(BuildContext context){
-        return(
-            Scaffold(
-                appBar: AppBar(
-                    title: Text("test"),
-                    centerTitle: true,
-                    backgroundColor: Colors.blue,
-                ),
-                body: Text("test"),
-                bottomNavigationBar: BottomNavigationBar(
-                    items: const [
-                        
-                    ]
-                )
-            )
-        );
-    }
+  const HomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return (Scaffold(
+        appBar: AppBar(
+          title: Text("test"),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: Text("test"),
+        bottomNavigationBar: BottomNavigationBar(items: const [])));
+  }
 }
