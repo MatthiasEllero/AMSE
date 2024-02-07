a26d6b72a53a0e0aa7afa532be9a64f57834a17a
achraf205 <achrafouhadach@gmail.com>
2024-02-07 15:28:28 +0100

    modifier media

--- TP1/lib/media.dart
+++ TP1/lib/media.dart
@@ -1,13 +1,13 @@
 class Media {
-    String name;
-    String genre;
-    String year;
-    String image_url;
+  String name;
+  String genre;
+  String year;
+  String image_url;
 
-    Media(this.name,this.genre,this.year,this.image_url);
+  Media(this.name, this.genre, this.year, this.image_url);
 }
 
-List<Media> listeMedia = [
-    Media("Inception","film","2010","test"),
-    Media("Inception","film","2010","test"),
-    ];
+var listeMedia = [
+  Media("Inception", "film", "2010", "test"),
+  Media("Inception", "film", "2010", "test"),
+];
