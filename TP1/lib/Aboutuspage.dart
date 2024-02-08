2e4d49b632f5e876dee6001ce268fd81e5b8f98e
achraf205 <achrafouhadach@gmail.com>
2024-02-08 18:29:01 +0100

    homepage modified

--- TP1/lib/Aboutuspage.dart
+++ TP1/lib/Aboutuspage.dart
@@ -11,59 +11,56 @@ class AboutUsPage extends StatelessWidget {
         title: const Text(
             'About Us - AMSE TV'), // Modification du titre pour plus de spécificité
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
+      body: const SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            const Text(
+            Text(
               'About us',
               style: TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight
                       .bold), // Ajustement de la taille de police pour correspondre à HomePage
             ),
-            const SizedBox(
-                height: 20), // Augmentation de l'espace après le titre
-            const Text(
+            SizedBox(height: 20), // Augmentation de l'espace après le titre
+            Text(
               'At Amse TV, we\'re passionate about bringing you the richest selection of films, series, and mangas from around the globe. Our mission is to provide easy access to a world of stories, where every user can find something that resonates.',
               style: TextStyle(fontSize: 16),
             ),
-            const SizedBox(height: 20),
-            const Text(
+            SizedBox(height: 20),
+            Text(
               'Our mission is to make diverse and compelling multimedia content accessible to everyone, anywhere, anytime. We envision a world where stories transcend borders, fostering understanding and connection across cultures.',
               style: TextStyle(fontSize: 16),
             ),
-            const SizedBox(height: 20),
-            const Text(
+            SizedBox(height: 20),
+            Text(
               'Our team is made up of passionate students, working tirelessly to curate and bring you the best content.',
               style: TextStyle(fontSize: 16),
             ),
-            const SizedBox(height: 20),
-            const Text(
+            SizedBox(height: 20),
+            Text(
               'We love hearing from you!',
               style: TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight
                       .bold), // Ajustement de la taille de police pour correspondre à HomePage
             ),
-            const SizedBox(height: 20),
-            const Text(
+            SizedBox(height: 20),
+            Text(
               'For support, feedback, or inquiries, please contact us at:',
               style: TextStyle(fontSize: 16),
             ),
-            const SizedBox(height: 10),
+            SizedBox(height: 10),
             SelectableText(
               'achraf.ouhadach@etu.imt-nord-europe.fr',
               style: TextStyle(fontSize: 16, color: Colors.blue),
               // Vous pouvez ajouter du code ici pour implémenter la fonction de copie ou d'ouverture de l'application de messagerie
             ),
-            const SizedBox(
-                height: 10), // Ajout d'un espace entre les adresses e-mail
+            SizedBox(height: 10), // Ajout d'un espace entre les adresses e-mail
             SelectableText(
               'matthias.ellero@etu.imt-nord-europe.fr',
               style: TextStyle(fontSize: 16, color: Colors.blue),
-              // Vous pouvez ajouter du code ici pour implémenter la fonction de copie ou d'ouverture de l'application de messagerie
             ),
           ],
         ),
--- TP1/lib/homePage.dart
+++ TP1/lib/homePage.dart
@@ -6,23 +6,24 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('AMSE TV'),
+        title: const Text('AMSE TV'),
       ),
       body: SingleChildScrollView(
         // Permet le défilement
-        padding: EdgeInsets.all(16.0), // Ajoute un padding autour du contenu
+        padding:
+            const EdgeInsets.all(16.0), // Ajoute un padding autour du contenu
         child: Column(
           crossAxisAlignment:
               CrossAxisAlignment.start, // Alignement du texte au début
           children: <Widget>[
-            Text(
+            const Text(
               'Welcome to AMSE TV, Your Ultimate Multimedia Companion!',
               style: TextStyle(
                 fontSize: 24, // Taille de police pour le titre
                 fontWeight: FontWeight.bold, // Police en gras pour le titre
               ),
             ),
-            SizedBox(height: 20), // Espace entre le titre et l'image
+            const SizedBox(height: 20), // Espace entre le titre et l'image
             Image.network(
               'https://play-lh.googleusercontent.com/ZwNZF0FGnDrBK-OPoc5rcOMtb-q-SbwsupUpXVTroYdoXtD2BbtUCo-05B0c0gYgK6M', // Remplacez par votre URL d'image
               width: double
@@ -32,68 +33,70 @@ class HomePage extends StatelessWidget {
               fit: BoxFit
                   .cover, // Couvre l'espace alloué complètement, en coupant l'image si nécessaire
             ),
-            SizedBox(height: 20), // Espace entre l'image et le texte
-            Text(
+            const SizedBox(height: 20), // Espace entre l'image et le texte
+            const Text(
               'Dive into a vast sea of films, series, and mangas right at your fingertips. Explore the latest releases, timeless classics, and hidden gems tailored just for you. Whether you\'re in the mood for an epic adventure, a heartwarming story, or a thrilling mystery, your next favorite is just a tap away.',
               style: TextStyle(
                 fontSize: 16, // Taille de police pour le texte
               ),
             ),
-            SizedBox(height: 20), // Espace avant "What’s New?"
-            Text(
+            const SizedBox(height: 20), // Espace avant "What’s New?"
+            const Text(
               'What’s New?',
               style: TextStyle(
                 fontSize: 20, // Taille de police pour les titres de section
                 fontWeight: FontWeight.bold, // Titres de section en gras
               ),
             ),
-            SizedBox(height: 10), // Espace avant la description de la section
-            Text(
+            const SizedBox(
+                height: 10), // Espace avant la description de la section
+            const Text(
               'Discover what\'s trending, recent additions, and our top picks for you this week.',
               style: TextStyle(
                 fontSize: 16, // Taille de police pour le texte
               ),
             ),
-            SizedBox(height: 20), // Ajoutez un espace avant "Explore Genres"
-            Text(
+            const SizedBox(
+                height: 20), // Ajoutez un espace avant "Explore Genres"
+            const Text(
               'Explore Genres',
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 10),
-            Text(
+            const SizedBox(height: 10),
+            const Text(
               'From action-packed adventures to serene slice-of-life narratives, find content that matches your mood.',
               style: TextStyle(
                 fontSize: 16,
               ),
             ),
-            SizedBox(height: 20),
-            Text(
+            const SizedBox(height: 20),
+            const Text(
               'Your Favorites Await',
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 10),
-            Text(
+            const SizedBox(height: 10),
+            const Text(
               'Quick access to your Liked Multimedia. It\'s like we know you better than you know yourself.',
               style: TextStyle(
                 fontSize: 16,
               ),
             ),
-            SizedBox(height: 20),
-            Text(
+            const SizedBox(height: 20),
+            const Text(
               'Start Your Journey',
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 10),
-            Text(
+            const SizedBox(height: 10),
+            const Text(
               'Browse, discover, and enjoy. Welcome to a world where your next adventure is always ready to begin.',
               style: TextStyle(
                 fontSize: 16,
